78b8f5e0febbbef7b77b94967c38800b1d5d5e4a
cherez <snwallace@gmail.com>
2010-03-28 22:30:22 +0000

    Python client fix, massive rule changes.

--- server/game/plant.py
+++ server/game/plant.py
@@ -40,7 +40,7 @@ class Plant(MappableObject):
         self.flowerRootUp = 0
         self.flowerLeafUp = 0
         self.flowerFlowerUp = 0
-        self.canAct = 1
+        self.canAct = 0
 
     def toList(self):
         list = MappableObject.toList(self)
@@ -53,84 +53,110 @@ class Plant(MappableObject):
 
     @checkOwnership("grow a leaf")
     def buildLeaf(self):
+        if not self.canAct:
+            return str(self.id) + " has no actions."
         if(self.leaf):
-            return str(self.id) + "cannot grow a leaf; it already has a leaf."
-        else:
-            if self.owner.light >= Plant.leafCost:
-                    self.leaf = 1
-                    self.owner.light -= Plant.leafCost
-            else:
-                return str(self.id) + "cannot grow leaf; not enough resources: have %s, need %s" % (self.owner.light, Plant.leafCost)
+            return str(self.id) + " cannot grow a leaf; it already has a leaf."
+        if self.owner.light < Plant.leafCost:
+            return str(self.id) + " cannot grow leaf; not enough resources: have %s, need %s" % (self.owner.light, Plant.leafCost)
+        
+        self.leaf = 1
+        self.owner.light -= Plant.leafCost
+        self.canAct -= 1
         return True
 
     @checkOwnership("grow a root")
     def buildRoot(self):
-        if(self.root):
+        if not self.canAct:
+            return str(self.id) + " has no actions."
+        if self.root:
             return str(self.id) + " cannot grow roots; it already has roots."
-        else:
-            if( self.owner.light >= Plant.rootCost ): 
-                self.root = 1
-                self.owner.light -= Plant.rootCost
-            else:
-                return str(self.id) + " cannot grow root; not enough resources: have %s, need %s" % (self.owner.light, Plant.rootCost)
+        if self.owner.light < Plant.rootCost: 
+            return str(self.id) + " cannot grow root; not enough resources: have %s, need %s" % (self.owner.light, Plant.rootCost)
+        
+        self.root = 1
+        self.heath += self.techLevelRoot
+        self.owner.light -= Plant.rootCost
+        self.canAct -= 1
         return True
 
     @checkOwnership("grow a flower")
     def buildFlower(self, rootUp, leafUp, flowerUp):
+        if not self.canAct:
+            return str(self.id) + " has no actions."
         if(self.flower):
             return str(self.id) + "cannot grow a flower; It already has a flower."
         if rootUp < 0 or leafUp < 0 or flowerUp < 0:
             return str(self.id) + " cannot have negative 'up' values."
         if rootUp + leafUp + flowerUp > self.techLevelFlower:
-            return str(self.id) + " does not have enough resources to grow" + \
-                " a flower with these features."
-        else:
-            if( self.owner.light >= Plant.flowerCost ):
-                self.flower = 1
-                self.owner.light -= Plant.flowerCost
-            else:
-                return str(self.id) + "cannot grow flower; not enough resources: have %s, need %s" % (self.owner.light, Plant.flowerCost)
+            return str(self.id) + " flower enhancements exceed flower level"
+        if self.owner.light < Plant.flowerCost:
+            return str(self.id) + "cannot grow flower; not enough resources: have %s, need %s" % (self.owner.light, Plant.flowerCost)
+        
+        self.flower = 1
+        self.owner.light -= Plant.flowerCost
         self.flowerRootUp = rootUp
         self.flowerLeafUp = leafUp
         self.flowerFlowerUp = flowerUp
+        self.canAct -= 1
         return True
 
     @checkOwnership("spread")
     def spread(self, x, y):
-        if(not self.root):
+        cost = Plant.spreadCost * (self.techLevelRoot + self.techLevelLeaf + self.techLevelFlower)
+        if not self.canAct:
+            return str(self.id) + " has no actions."
+        if not self.root:
             return str(self.id) + " does not have a root, cannot spread"
-        if abs(x - self.x) + abs(y - self.y) == 1:
-            if self.game.area[(x, y)]:
-                return "Cannot spread; destination populated."
-            if(self.owner.light >= Plant.rootCost):
-                newPlant = Plant(self.game, x, y, self.owner)
-                newPlant.root = 1
-                newPlant.techLevelRoot = self.techLevelRoot
-                newPlant.techLevelLeaf = self.techLevelLeaf
-                newPlant.techLevelFlower = self.techLevelFlower
-                self.game.addObject(newPlant)
-            else:
-                return "Cannot spread; not enough resources: have %s, need %s" % (self.owner.light, Plant.rootCost)
-        else:
+        if not  abs(x - self.x) + abs(y - self.y) == 1:
             return "Coordinate not within range of plant"
+        try:
+            self.game.area[(x, y)]
+        except IndexError:
+            return "Cannot spread; outside the world."
+            
+        if self.owner.light < Plant.cost:
+            return "Cannot spread; not enough resources: have %s, need %s" % (self.owner.light, cost)
+        
+        self.owner.light -= cost
+        
+        if self.game.area[(x, y)]:
+            target = self.game.area[(x, y)][0]
+            target.health -= self.techLevelRoot
+        else:
+            newPlant = Plant(self.game, x, y, self.owner)
+            newPlant.techLevelRoot = self.techLevelRoot
+            newPlant.techLevelLeaf = self.techLevelLeaf
+            newPlant.techLevelFlower = self.techLevelFlower
+            self.game.addObject(newPlant)
+        
+        self.canAct -= 1
+        return True
 
     @checkOwnership("spawn")
     def spawn(self, x, y):
+        cost = Plant.spawnCost * (self.techLevelRoot + self.techLevelLeaf + self.techLevelFlower + self.flowerRootUp + self.flowerLeafUp + self.flowerFlowerUp)
+        if not self.canAct:
+            return str(self.id) + " has no actions."
         if(not self.flower):
             return str(self.id) + " does not have a flower, cannot spawn"
-        if abs(x - self.x) + abs(y - self.y) == 1:
+        if not abs(x - self.x) + abs(y - self.y) == 1:
+            return "Cannot spawn here; it's not within range"
+        try:
             if self.game.area[(x, y)]:
                 return "Cannot spawn; destination populated."
-            if(self.owner.light >= Plant.rootCost):
-                newPlant = Plant(self.game, x, y, self.owner)
-                newPlant.techLevelRoot = self.techLevelRoot + self.flowerRootUp
-                newPlant.techLevelLeaf = self.techLevelLeaf + self.flowerLeafUp
-                newPlant.techLevelFlower = self.techLevelFlower + self.flowerFlowerUp
-                self.game.addObject(newPlant)
-            else:
-                return "Cannot spawn; not enough resources: have %s, need %s" % (self.owner.light, Plant.rootCost)
-        else:
-            return "Cannot spawn here; it's not within range"
+        except IndexError:
+            return "Cannot spawn; outside the wold."
+        if self.owner.light < cost:
+            return "Cannot spawn; not enough resources: have %s, need %s" % (self.owner.light, cost)
+        
+        self.owner.light -= cost
+        newPlant = Plant(self.game, x, y, self.owner)
+        newPlant.techLevelRoot = self.techLevelRoot + self.flowerRootUp
+        newPlant.techLevelLeaf = self.techLevelLeaf + self.flowerLeafUp
+        newPlant.techLevelFlower = self.techLevelFlower + self.flowerFlowerUp
+        self.game.addObject(newPlant)
+        self.canAct -= 1
         return True
 
     @checkOwnership("talk")
@@ -140,27 +166,22 @@ class Plant(MappableObject):
 
     def nextTurn(self):
         MappableObject.nextTurn(self)
-        if (self.owner == self.game.turn and self.canAct):
-            self.owner.light += self.techLevelLeaf
-            #self.light += techLevelLeaf
-            self.owner.score += self.techLevelLeaf
+        if (self.owner == self.game.turn):
+            self.owner.light += self.techLevelLeaf * self.leaf  + Plant.minLight
+            self.owner.score += self.techLevelLeaf * self.leaf + Plant.minLight
 
             self.health -= 1
-
-            if(self.root and self.leaf and self.flower):
-                self.health -= (Plant.rootHealthCost + Plant.leafHealthCost + Plant.flowerHealthCost)
-            elif(self.root and self.leaf):
-                self.health -= (Plant.rootHealthCost + Plant.leafHealthCost)
-            elif(self.root and self.flower):
-                self.health -= (Plant.rootHealthCost + Plant.flowerHealthCost)
-            elif(self.leaf and self.flower):
-                self.health -= (Plant.leafHealthCost + Plant.flowerHealthCost)
-            elif(self.leaf):
+            
+            if(self.leaf):
                 self.health -= (Plant.leafHealthCost)
-            elif(self.root):
+            if(self.root):
                 self.health -= (Plant.rootHealthCost)
-            elif(self.flower):
+            if(self.flower):
                 self.health -= (Plant.flowerHealthCost)
 
             if ( self.health <= 0 ):
                 self.game.removeObject(self)
+            
+            self.canAct = 0
+        else:
+            self.canAct = 1
